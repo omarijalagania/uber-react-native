88de2e0875af3f38fb337c6ee301bfb787237f83
omari <omari.jalagania@gmail.com>
2022-02-05 22:08:32 +0400

    map items

--- components/Restaurant/RestaurantItem.js
+++ components/Restaurant/RestaurantItem.js
@@ -2,22 +2,57 @@ import { View, Text, Image, TouchableOpacity } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import React from "react";
 
+const localRestaurants = [
+  {
+    name: "Beachside Bar",
+    image_url:
+      "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1244,
+    rating: 4.5,
+  },
+  {
+    name: "Benihana",
+    image_url:
+      "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1224,
+    rating: 3.7,
+  },
+  {
+    name: "India's Grill",
+    image_url:
+      "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    categories: ["Indian", "Bar"],
+    price: "$$",
+    reviews: 700,
+    rating: 4.9,
+  },
+];
+
 const RestaurantItem = () => {
   return (
     <TouchableOpacity activeOpacity={1} style={{ marginBottom: 30 }}>
-      <View style={{ marginTop: 7, padding: 15, backgroundColor: "white" }}>
-        <RestaurantImage />
-        <RestarauntInfo />
-      </View>
+      {localRestaurants.map((restaurant) => (
+        <View
+          key={restaurant.reviews}
+          style={{ marginTop: 7, padding: 15, backgroundColor: "white" }}
+        >
+          <RestaurantImage imageUrl={restaurant.image_url} />
+          <RestarauntInfo name={restaurant.name} rating={restaurant.rating} />
+        </View>
+      ))}
     </TouchableOpacity>
   );
 };
 
-const RestaurantImage = () => (
+const RestaurantImage = ({ imageUrl }) => (
   <>
     <Image
       source={{
-        uri: "https://images.pexels.com/photos/67468/pexels-photo-67468.jpeg",
+        uri: imageUrl,
       }}
       style={{ width: "100%", height: 180 }}
     />
@@ -27,7 +62,7 @@ const RestaurantImage = () => (
   </>
 );
 
-const RestarauntInfo = () => (
+const RestarauntInfo = ({ name, rating }) => (
   <View
     style={{
       flexDirection: "row",
@@ -36,9 +71,7 @@ const RestarauntInfo = () => (
     }}
   >
     <View>
-      <Text style={{ fontSize: 15, fontWeight: "bold" }}>
-        Formhouse Kitchen
-      </Text>
+      <Text style={{ fontSize: 15, fontWeight: "bold" }}>{name}</Text>
       <Text style={{ fontSize: 13, color: "gray" }}>40-45 * min</Text>
     </View>
     <View
@@ -51,7 +84,7 @@ const RestarauntInfo = () => (
         justifyContent: "center",
       }}
     >
-      <Text>4.5</Text>
+      <Text>{rating}</Text>
     </View>
   </View>
 );
