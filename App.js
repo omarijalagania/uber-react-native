c15c8f5fe1049bada06d743f46ac511ccfcbeae8
omari <omari.jalagania@gmail.com>
2022-02-05 21:08:35 +0400

    categories

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
-import { View, Text, SafeAreaView } from "react-native";
-import HeaderTabs from "./components/Header/HeaderTabs";
-import Searchbar from "./components/Search/Searchbar";
+import Home from "./screens/Home";
 
 export default function App() {
-  return (
-    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
-      <View style={{ backgroundColor: "white", padding: 10 }}>
-        <HeaderTabs />
-        <Searchbar />
-      </View>
-    </SafeAreaView>
-  );
+  return <Home />;
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,16 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+import { View, Text, SafeAreaView } from "react-native";
+import Categories from "../components/Categories/Categories";
+import HeaderTabs from "../components/Header/HeaderTabs";
+import Searchbar from "../components/Search/Searchbar";
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 10 }}>
+        <HeaderTabs />
+        <Searchbar />
+      </View>
+      <Categories />
+    </SafeAreaView>
   );
 };
 
